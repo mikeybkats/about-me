b96b4fee9acf79d12f656be7f7d4eb4265834c71
Michael Shipley Barakat <michaebkats@gmail.com>
2016-05-10 21:45:43 -0700

    update to app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 //this is a comment
-document.getElementById('quizbutton').onclick = function(){
-  quiz();
-};
 
 function quiz() {
   var username = prompt('Hi dude, thanks for visiting. What\'s your name slugger?');
@@ -14,14 +11,14 @@ function quiz() {
 
   if (response1 === answer1 || response1 === answer1[0]) {
     alert('good job. you got it right ' + username + '. Music is my favorite hobby.');
-    console.log('user response1 = true');
     response1 = true;
+    console.log(response1);
   }
 
   else {
     alert('Nope,' + username + '.' + ' music is my favorite hobby.');
-    console.log('user response1 = false');
     response1 = false;
+    console.log(response1);
   }
 
   var response2 = prompt('Do I come from the the big city?').toLowerCase();
@@ -30,13 +27,14 @@ function quiz() {
 
   if (response2 === answer2 || response2 === answer2[0]){
     alert('Good job! You are correct ' + username + '. I do not come from the big city and in fact hale from a small town in the country.' );
-    console.log('user response2 = true');
-    response2 = true;
+    var response2 = true;
+    console.log(response2);
   }
+
   else {
     alert('Actually, no. ' + username + '. I come from a small country town.' );
-    console.log('user response 2 = false');
-    response2 = false;
+    var response2 = false;
+    console.log(response2);
   }
 
   var response3 = prompt('Is my favorite color gray?').toLowerCase();
@@ -45,13 +43,14 @@ function quiz() {
 
   if (response3 === answer3 || response3 === answer3[0]){
     alert('Nice. That\'s right. My favorite color is gray.');
-    console.log('user response3 = true');
     response3 = true;
+    console.log(response3);
   }
+
   else {
     alert('That\'s wrong ' + username + '. My favorite color is gray.');
-    console.log('user response3 = false');
     response3 = false;
+    console.log(response3);
   }
 
   var response4 = prompt('Is it true that I love sports and weightlifting?').toLowerCase();
@@ -60,13 +59,13 @@ function quiz() {
 
   if (response4 === answer4 || response4 === answer4[0]){
     alert('Very good ' + username + '. You\'re very clever, and obviously picked up on the fact that the last question was a curveball. I do like weightlifting, but I am no sports buff.');
-    console.log('user response4 = true');
-    response4 = true;
+    var response4 = true;
+    console.log(response4);
   }
   else {
     alert(username + ', you\'re going to have to try harder than that.' );
-    console.log('user response4 = false');
     response4 = false;
+    console.log(response4);
   }
 
   var response5 = prompt('I prefer things that are old. Yes or No?').toLowerCase();
@@ -75,12 +74,17 @@ function quiz() {
 
   if (response5 === answer5 || response5 === answer5[0]){
     alert('Woah! Yep! That\'s right');
-    console.log('The user response is correct');
+    response5 = true;
+    console.log(response5);
   }
   else{
     alert(username + ' sorry, that\'s not the right answer');
-    console.log('user response5 = false');
-    response5 = false;
+    var response5 = false;
+    console.log(response5);
   }
 
+};
+
+document.getElementById('quizbutton').onclick = function(){
+  quiz();
 };
