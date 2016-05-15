b6968888088c8fb3dfd4a4fadb42d48460ad76f1
Michael Shipley Barakat <michaebkats@gmail.com>
2016-05-15 15:05:56 -0700

    made updates to app.js to finish the assignments

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-//this is a comment
 function quiz() {
 
-  function username (){
-    var username = prompt('Hi dude, thanks for visiting. What\'s your name slugger?');
-    alert('Nice to meet you ' + username + '. I\'m going to give you a quiz about me. A little game called: How well do you know me');
-    console.log('The user\'s name is ' + username + '.');
-  }
-  function questionOne () {
+  var questionTally = 0;
+
+  var username = prompt('Hi dude, thanks for visiting. What\'s your name slugger?');
+  alert('Nice to meet you ' + username + '. I\'m going to give you a quiz about me. A little game called: How well do you know me');
+  console.log('The user\'s name is ' + username + '.');
+
+  function questionSetOne (){
+    //first question
     var response1 = prompt('Is music my favorite hobby?').toLowerCase();
     var answer1 = 'yes';
     console.log('The user has responded ' + response1 + ' to the first question');
@@ -15,6 +16,7 @@ function quiz() {
       alert('good job. you got it right ' + username + '. Music is my favorite hobby.');
       response1 = true;
       console.log(response1);
+      questionTally = questionTally += 1;
     }
     else {
       alert('Nope,' + username + '.' + ' music is my favorite hobby.');
@@ -22,6 +24,7 @@ function quiz() {
       console.log(response1);
     }
 
+    //second question
     var response2 = prompt('Do I come from the the big city?').toLowerCase();
     var answer2 = 'no';
     console.log('The user has responded ' + response2 + ' to the second question.');
@@ -30,6 +33,7 @@ function quiz() {
       alert('Good job! You are correct ' + username + '. I do not come from the big city and in fact hale from a small town in the country.' );
       var response2 = true;
       console.log(response2);
+      questionTally = questionTally += 1;
     }
     else {
       alert('Actually, no. ' + username + '. I come from a small country town.' );
@@ -37,6 +41,7 @@ function quiz() {
       console.log(response2);
     }
 
+    //Third question
     var response3 = prompt('Is my favorite color gray?').toLowerCase();
     var answer3 = 'yes';
     console.log('The user has responded ' + response3 + ' to the third question.');
@@ -45,6 +50,7 @@ function quiz() {
       alert('Nice. That\'s right. My favorite color is gray.');
       response3 = true;
       console.log(response3);
+      questionTally = questionTally += 1;
     }
     else {
       alert('That\'s wrong ' + username + '. My favorite color is gray.');
@@ -52,6 +58,7 @@ function quiz() {
       console.log(response3);
     }
 
+    //Fourth question
     var response4 = prompt('Is it true that I love sports and weightlifting?').toLowerCase();
     var answer4 = 'no';
     console.log('The user has responded ' + response4 + ' to the fourth question.');
@@ -60,6 +67,7 @@ function quiz() {
       alert('Very good ' + username + '. You\'re very clever, and obviously picked up on the fact that the last question was a curveball. I do like weightlifting, but I am no sports buff.');
       var response4 = true;
       console.log(response4);
+      questionTally = questionTally += 1;
     }
     else {
       alert(username + ', you\'re going to have to try harder than that.' );
@@ -67,6 +75,7 @@ function quiz() {
       console.log(response4);
     }
 
+    //Fifth question
     var response5 = prompt('I prefer things that are old. Yes or No?').toLowerCase();
     var answer5 = 'yes';
     console.log('The user has responded ' + response5 + ' to the fifth question.');
@@ -75,28 +84,25 @@ function quiz() {
       alert('Woah! Yep! That\'s right');
       response5 = true;
       console.log(response5);
+      questionTally = questionTally += 1;
     }
     else{
       alert(username + ' sorry, that\'s not the right answer');
       var response5 = false;
       console.log(response5);
-    }}
-  //
-  // /* As a developer, I want to add a sixth question to my guessing game that takes numeric input,
-  //  and indicates to the user whether the guess is 'too high' or 'too low', and gives the user exactly
-  // four opportunities to get the correct answer, so that my fancy programming skills are showcased.
-  // 1 - for loop counts from 1 to 5. If i = 1 then run x, if i = 2 then run x, if i = 3 then run x, if i = 5 then alert.
-  // */
-  function guessShoeSize (){
+    }
+  }
+
+  function questionSetTwo (){
+    //Sixth question
     var shoeSize = 11;
     var guesses = 4;
-    var shoeSizeQuestion = parseInt(prompt('Okay, ' + username + ' let\'s see how good you really are. Can you guess my shoe size in four attempts?'));
+    var shoeSizeQuestion = parseInt(prompt('Okay, ' + username + ' let\'s see how good you really are. Can you guess my shoe size in four attempts?')) || 0;
     guesses = guesses -= 1;
     var correctSize = true;
 
     while (correctSize === true){
       console.log(shoeSizeQuestion);
-
       if (shoeSizeQuestion < shoeSize ){
         if (guesses > 1){
           shoeSizeQuestion = parseInt(prompt( 'It looks like your guess is a little too low. Try a higher number. You have ' + guesses + ' guesses left.')) || 0;
@@ -109,10 +115,10 @@ function quiz() {
 
       if (shoeSizeQuestion > shoeSize ){
         if (guesses > 1){
-          shoeSizeQuestion = parseInt(prompt( 'It looks like your guess is a little too high. Try a lower number and make it good! This is your last guess.' )) || 0;
+          shoeSizeQuestion = parseInt(prompt( 'It looks like your guess is a little too high. Try a lower number. You have ' + guesses + ' guess left.')) || 0;
         }
         else if (guesses === 1){
-          shoeSizeQuestion = parseInt(prompt( 'It looks like your guess is a little too low. Try a lower number. You have ' + guesses + ' guess left.')) || 0;
+          shoeSizeQuestion = parseInt(prompt( 'It looks like your guess is a little too high. Try a lower number and make it good! This is your last guess.' )) || 0;
         }
         guesses = guesses -= 1;
       }
@@ -121,64 +127,19 @@ function quiz() {
         alert('Very good, ' + username + ', you have guessed the correct size!');
         guesses = guesses += 1;
         var correctSize = false;
-      }
-
-      function questionSeven(){
-        var questionSeven = prompt('Try guessing one of my favorite colors in six tries?');
-        var nextGuess = 'Try guessing again.';
-        var colorArray = ['gray', 'orange', 'mint green', 'purple', 'cerulean', 'eggplant'];
-        var correctAnswer = false;
-        var runAgain = false;
-        for (var i = 0; i < colorArray.length; i++){
-          if (runAgain === true){
-            var questionSeven = prompt('Try guessing again.');
-            if (questionSeven === colorArray[i]){
-              console.log('the user chose the color ' + questionSeven);
-              console.log('the user selection is not ' + correctAnswer);
-              correctAnswer = true;
-              console.log('the user selection is ' + correctAnswer);
-              alert('You have guessed correctly. Great job ' + username + '.');
-              break;
-            }
-            else {
-              alert('Wrong answer. You now have ' + (5 - i) + ' tries left.');
-              var runAgain = true;
-              if ((5 - i) === 0){
-                alert('You have lost. Sorry ' + username + '.');
-                break;
-              }
-            }
-          }
-          if (runAgain === false){
-            if (questionSeven === colorArray[i]){
-              console.log('the user chose the color ' + questionSeven);
-              console.log('the user selection is not ' + correctAnswer);
-              correctAnswer = true;
-              console.log('the user selection is ' + correctAnswer);
-              alert('You have guessed correctly. Great job ' + username + '.');
-              break;
-            }
-            else {
-              alert('Wrong answer. You now have ' + (5 - i) + ' tries left.');
-              var runAgain = true;
-              if ((5 - i) === 0){
-                alert('You have lost. Sorry ' + username + '.');
-                break;
-              }
-            }
-          }
-        }
+        questionTally = questionTally += 1;
       }
 
       if (guesses === 0){
-        alert('You have lost the game.');
-        correctSize = false;
+        alert('Sorry ' + username + ' you have failed to guess the my size. Which is 11 BTW. ');
+        var correctSize = false;
       }
     }
   }
 
-  function questionSeven(){
-    var youWin = 'Great job. You won the game.';
+  function questionSetThree(){
+    //Seventh question
+    var youWin = 'Great job. You guessed one of my favorite colors!';
     var colorArray = ['gray', 'orange', 'mint green', 'purple', 'cerulean', 'eggplant'];
     var correctAnswer = false;
     var runAgain = false;
@@ -194,6 +155,7 @@ function quiz() {
         if (questionSeven === colorArray[i]){
           alert(youWin);
           correctAnswer = true;
+          questionTally = questionTally += 1;
           break;
         }
       }
@@ -203,23 +165,19 @@ function quiz() {
         runAgain = true;
       }
       if (y === 0){
-        alert('You have lost. Thanks for playing!');
+        alert('You have failed to guess one of my favorite colors. Well... here they are in case you were interested: Gray, purple, orange, mint green, cerulean, eggplant (which is really just dark purple). =)');
       }
     }
   }
 
   function endingMessage (){
-    alert('That concludes our quiz. Thanks for playing!');
+    alert('That concludes our quiz. Thanks for playing! Your got ' + questionTally + ' out of a possible ' + 7 + ' questions correct.' );
   }
 
-  username();
-  questionOne();
-  guessShoeSize();
-  questionSeven();
+  questionSetOne();
+  questionSetTwo();
+  questionSetThree();
   endingMessage();
-
 }
 
-document.getElementById('quizbutton').onclick = function(){
-  quiz();
-};
+document.getElementById('quizbutton').onclick = function(){ quiz(); };
--- app_test.js
+++ app_test.js
@@ -1,51 +1,7 @@
-//this is a comment
+//this is a scratch pad for project tests and ideas
 
 function quiz() {
-  // var username = prompt('Hi dude, thanks for visiting. What\'s your name slugger?');
-  // alert('Nice to meet you ' + username + '. I\'m going to give you a quiz about me. A little game called: How well do you know me?');
-  // console.log('The user\'s name is ' + username + '.');
 
-  function questionSeven(){
-    var questionSeven = prompt('Try guessing one of my favorite colors in six tries?');
-    var nextGuess = 'Try guessing again.';
-    var youWin = 'Great job. You won the game.';
-    var colorArray = ['gray', 'orange', 'mint green', 'purple', 'cerulean', 'eggplant'];
-    var correctAnswer = false;
-    var repeatGuess = false;
-    for (var i = 0; i < colorArray.length; i++){
-      if (correctAnswer === false){
-        console.log(colorArray[i]);
-        if(questionSeven === colorArray[i]){
-          correctAnswer = true;
-        }
-        else if (questionSeven !== colorArray[i]){
-          correctAnswer = false;
-        }
-      }
-      else if (correctAnswer === true){
-        alert(youWin);
-      }
-    }
-    // for (var i = 0; i < colorArray.length; i++){
-    //   if (correctAnswer === true){
-    //     var questionSeven = prompt('Try guessing again. You now have ' + (colorArray.length - i) + ' tries left.');
-    //     correctAnswer = false;
-    //   }
-    //   else if (correctAnswer === false){
-    //     console.log(colorArray[i]);
-    //     if(questionSeven === colorArray[i]){
-    //       alert(youWin);
-    //     }
-    //     else {
-    //       var correctAnswer = true;
-    //     }
-    //   }
-    //   else {
-    //     alert('Try again.');
-    //     var correctAnswer = true;
-    //   }
-    // }
-  }
   questionSeven();
 }
 document.getElementById('quizbutton').onclick = function(){
