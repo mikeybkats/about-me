b9cd54953ceeacb2492d9723e1ff7215e1269c1d
Michael Shipley Barakat <michaebkats@gmail.com>
2016-05-14 17:01:08 -0700

    updating to new version

--- app_test.js
+++ app_test.js
@@ -5,49 +5,61 @@ function quiz() {
   // alert('Nice to meet you ' + username + '. I\'m going to give you a quiz about me. A little game called: How well do you know me?');
   // console.log('The user\'s name is ' + username + '.');
 
-  function questionSeven(){
-    var questionSeven = prompt('Try guessing one of my favorite colors in six tries?');
-    var nextGuess = 'Try guessing again.';
-    var youWin = 'Great job. You won the game.';
-    var colorArray = ['gray', 'orange', 'mint green', 'purple', 'cerulean', 'eggplant'];
-    var correctAnswer = false;
-    var repeatGuess = false;
-    for (var i = 0; i < colorArray.length; i++){
-      if (correctAnswer === false){
-        console.log(colorArray[i]);
-        if(questionSeven === colorArray[i]){
-          correctAnswer = true;
-        }
-        else if (questionSeven !== colorArray[i]){
-          correctAnswer = false;
-        }
-      }
-      else if (correctAnswer === true){
-        alert(youWin);
+  function guessShoeSize (){
+    var username = 'cob';
+    var shoeSize = 11;
+    var guesses = 4;
+    console.log(shoeSizeQuestion);
+    var correctSize = true;
+    while (correctSize === true){
+      var shoeSizeQuestion = parseFloat(prompt('Okay, ' + username + ' let\'s see how good you really are. Can you guess my shoe size in four attempts?'));
+      console.log(shoeSizeQuestion);
+      // if (shoeSizeQuestion < shoeSize ){
+      //   guesses = guesses -= 1;
+      //   if (guesses > 0){
+      //     shoeSizeQuestion = prompt( 'Try a higher number. You have ' + guesses + ' guesses left.');
+      //     console.log(shoeSizeQuestion);
+      //   }
+      //   if (guesses === 0){
+      //     shoeSizeQuestion = prompt('This is your last chance, make it a good guess!');
+      //     console.log(shoeSizeQuestion);
+      //   }
+      // }
+      // if (shoeSizeQuestion > shoeSize ){
+      //   guesses = guesses -= 1;
+      //   if (guesses > 0){
+      //     shoeSizeQuestion = prompt( 'Try a lower number. You have ' + guesses + ' guesses left.' );
+      //     console.log(shoeSizeQuestion);
+      //   }
+      //   else if (guesses === 0){
+      //     shoeSizeQuestion = prompt('This is your last chance, make it a good guess!');
+      //     console.log(shoeSizeQuestion);
+      //   }
+      // }
+
+      if (shoeSizeQuestion === shoeSize) {
+        alert('Very good, ' + username + ', you have guessed the correct size!');
+        var correctSize = false;
+        break;
       }
     }
-    // for (var i = 0; i < colorArray.length; i++){
-    //   if (correctAnswer === true){
-    //     var questionSeven = prompt('Try guessing again. You now have ' + (colorArray.length - i) + ' tries left.');
-    //     correctAnswer = false;
-    //   }
-    //   else if (correctAnswer === false){
-    //     console.log(colorArray[i]);
-    //     if(questionSeven === colorArray[i]){
-    //       alert(youWin);
-    //     }
-    //     else {
-    //       var correctAnswer = true;
-    //     }
-    //   }
-    //   else {
-    //     alert('Try again.');
-    //     var correctAnswer = true;
-    //   }
-    // }
   }
-  questionSeven();
+  guessShoeSize();
 }
+
 document.getElementById('quizbutton').onclick = function(){
   quiz();
 };
+
+function questionSeven(){
+  var test = true;
+  var i = 10;
+
+  while( test === true){
+    i++;
+    console.log(i);
+    if (i === 200){
+      var test = false;
+    }
+  }
+}
